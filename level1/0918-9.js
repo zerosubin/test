d567e6b634b736217745e8c4272b0d041b147d7e
subin <psb2638@naver.com>
2023-09-18 23:48:49 +0900

    test: 문자열 내림차순으로 배치하기

---
+++ level1/0918-9.js
@@ -0,0 +1,16 @@
+// 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+// s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+
+function solution(s) {
+  var answer = ''
+  let last = s.split('')
+  last.sort(function(a, b){
+      if(a < b) return 1
+      if(a > b) return -1
+      if(a === b) return 0
+  })
+  answer += last.join('')
+  return answer
+}
+
+console.log(solution("Zbcdefg"))
