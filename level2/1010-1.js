b97a742728fe2dac3319424c36bd91d6b62857ee
subin <psb2638@naver.com>
2023-10-10 21:26:30 +0900

    test: 예상 대진표:

---
+++ level2/1010-1.js
@@ -0,0 +1,13 @@
+function solution(n,a,b)
+{
+    let cnt = 1
+    // 소수점을 만나면 무조건 올림해준다
+    while(Math.ceil(a/2) !== Math.ceil(b/2)){
+        a = Math.ceil(a/2)
+        b = Math.ceil(b/2)
+        cnt++
+    }
+    
+    return cnt
+}
+
